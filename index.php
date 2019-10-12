ffca8c0e08dc4a9ab96304069d61b6d403db5b0e
serviktor050 <mr.s-gagulin@yandex.ru>
2019-10-12 17:33:55 +0300

    В основной файл добавлено 2 занятие

--- index.php
+++ index.php
@@ -15,6 +15,14 @@
                     <li><a href="1.1-intro-and-branching/1.1.2">Задание 1.1.2</a></li>
                 </ul>
             </li>
+            <li>Занятие 1.2. Работа с формами и строками
+                <ul>
+                    <li><a href="1.2-strings-and-forms/1.2.1-registration">Задание 1 - Регистрация</a></li>
+                    <li><a href="1.2-strings-and-forms/1.2.2-name-standart">Задание 2 - Стандартизатор имени</a></li>
+                    <li><a href="1.2-strings-and-forms/1.2.3-additional">Задание 3 - Дополнительные задания</a></li>
+                </ul>
+
+            </li>
         </ul>
     </li>
 </ul>
