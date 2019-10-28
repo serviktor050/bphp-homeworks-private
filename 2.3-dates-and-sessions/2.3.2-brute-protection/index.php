d63aba4a12224be4bc4982baee61777c5f0a2f00
serviktor050 <mr.s-gagulin@yandex.ru>
2019-10-28 18:12:50 +0300

    Внесены корректировки по задаче 2

--- 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
+++ 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
@@ -5,43 +5,38 @@
         'janitor' => 'nimbus2000'
     ];
 
-    function login($users) {
-        if (isset($_POST['login'])) {
-            if ($users[$_POST['login']] === $_POST['password']) { 
-                echo 'Вход выполнен.';
-                return true;
-            } else {
-                echo 'Неверный логин/пароль.';
-                return false;
-            };
-        };
-    };
+    session_start();
 
-    $loginUser = login($users);
-
-    if ($loginUser === false) {
-        session_start();
-        $_SESSION['counter'];
-        
+    if (isset($_POST['login'])) {
         setcookie('login', $_POST['login']);
         setcookie('time', time());
 
-        if ($_COOKIE['login'] === $_POST['login'] && $_COOKIE['time']) {
-            if ((time() - $_COOKIE['time']) < 60) {
-                $_SESSION['counter']++;
-                if ($_SESSION['counter'] > 2) {
-                    echo '<br>Слишком часто вводите пароль. Попробуйте еще раз через минуту.';
-                    $_SESSION['counter'] = 0;
-                     
-                    $file = 'bruteForceFile.txt';
-                    $newFile = fopen($file, 'a+');
-                    $report ="Пользователь: ".$_COOKIE['login'].' '."Дата: ".date('d.m.Y H:i:s')."\n";
-                    fwrite($newFile, $report);
-                    fclose($newFile);
-                };            
-            };            
-            if ((time() - $_COOKIE['time']) < 5) {
-                echo '<br>Слишком часто вводите пароль.';
+        if ($users[$_POST['login']] === $_POST['password']) { 
+            echo 'Вход выполнен.';
+        } else {
+            $_SESSION['counter'];
+
+            if ($users[$_POST['login']] != $_POST['password'] && $_SESSION['counter'] <= 1) {
+                echo 'Вход не выполнен. Неверный логин/пароль.';
             };
+
+            if ($_COOKIE['login'] === $_POST['login'] && $_COOKIE['time']) {
+                if ((time() - $_COOKIE['time']) < 60) {
+                    $_SESSION['counter']++;
+                    if ($_SESSION['counter'] > 2) {
+                        echo 'Вход не выполнен. Неверный логин/пароль.<br>Слишком часто вводите пароль. Попробуйте еще раз через минуту.';
+                        $_SESSION['counter'] = 0;
+                         
+                        $file = 'bruteForceFile.txt';
+                        $newFile = fopen($file, 'a+');
+                        $report ="Пользователь: ".$_COOKIE['login'].' '."Дата: ".date('d.m.Y H:i:s')."\n";
+                        fwrite($newFile, $report);
+                        fclose($newFile);
+                    };            
+                };            
+                if ((time() - $_COOKIE['time']) < 5) {
+                    echo 'Вход не выполнен. Неверный логин/пароль.<br>Слишком часто вводите пароль.';
+                };
+            };             
         };
     };
